28531d3232974bc6bdae76fdd75385fea486337a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-01 04:34:45 +0000

    Deploy davisraimon/react-weather to github.com/davisraimon/react-weather.git:gh-pages

---
+++ precache-manifest.d486fc7c307e1562761253b811c7a24b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39fd42a4770499060a209a399ab2b2b9",
+    "url": "/react-weather/index.html"
+  },
+  {
+    "revision": "0e8938fde532b9b73c62",
+    "url": "/react-weather/static/css/2.ce71f937.chunk.css"
+  },
+  {
+    "revision": "a0d92e25d7cc6035792c",
+    "url": "/react-weather/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0e8938fde532b9b73c62",
+    "url": "/react-weather/static/js/2.1b8d98d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather/static/js/2.1b8d98d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0d92e25d7cc6035792c",
+    "url": "/react-weather/static/js/main.a70e6524.chunk.js"
+  },
+  {
+    "revision": "2f0b080eedbebc4ebf61",
+    "url": "/react-weather/static/js/runtime-main.10c92b92.js"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/react-weather/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/react-weather/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/react-weather/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/react-weather/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/react-weather/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
